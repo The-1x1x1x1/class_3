6bae71fb9772bfd233a91307d994efcdc08246fc
1x1x1x1 <nullandvoid186@gmail.com>
2025-09-01 18:36:07 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+name="John Doe"
+age= 35
+acc_banned= False
+acc_age=19.5
+
+print("data types of these are: ",type(name),type(age),type(acc_banned),type(acc_age))
+
+age=str(age)
+print("NEW data type of age is: ",type(age))
+
+acc_age=int(acc_age)
+print("NEW data type of account age is: ",type(acc_age))
